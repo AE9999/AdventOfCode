67ab6e6c0808e22e6f91aa20141421ecc472e263
blaat <blaat@hotmail.com>
2018-12-12 10:38:01 +0100

    Work as is

--- 12/sollution.py
+++ 12/sollution.py
@@ -2,6 +2,9 @@ import sys
 
 def list2int(list): return int("".join(str(i) for i in list), 2)
 
+def printState(state):
+    print("".join(["." if x == 0 else '#' for x in state]))
+
 state = [0,0] + list(map(lambda x: 0 if x == '.' else 1,
                      list(sys.stdin.readline().rstrip().split(' ')[2]))) + [0, 0]
 
@@ -9,11 +12,18 @@ transfers = dict(map(lambda x: (list2int(map(lambda x: 0 if x == '.' else 1, lis
                      map(lambda x: x.rstrip().split(' '),
                          filter(lambda x: len(x) > 2, sys.stdin.readlines()))))
 
+for x in range(32):
+    if x not in transfers.keys():
+        transfers[x] = 0
+
+print(transfers)
+
 def nextState(state):
     global transfers
-    return [0, 0] + [ state[(x-2):(x+2)] for x in range(2, len(state) - 2)] + [0, 0]
+    return [0, 0] + [ transfers[list2int(state[(x-2):(x+2)])] for x in range(2, len(state) - 2)] + [0, 0]
 pass
 
 for generation in range(20):
-    pass
+    printState(state)
+    state = nextState(state)
 pass
