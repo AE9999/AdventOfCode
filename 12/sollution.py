5f00f8dc012215ad42e35afea92c8ce5c15c67ba
blaat <blaat@hotmail.com>
2018-12-12 10:11:37 +0100

    Work as is.

--- 12/sollution.py
+++ 12/sollution.py
@@ -5,7 +5,7 @@ state = [0,0] + list(map(lambda x: 0 if x == '.' else 1,
 
 print (state)
 
-transfers = list(map(lambda x: (x[0], x[2]),
+transfers = list(map(lambda x: (map(lambda x: 0 if x == '.' else 1, list(x[0])), 0 if x[2] == '.' else 1),
                      map(lambda x: x.rstrip().split(' '),
                          filter(lambda x: len(x) > 2, sys.stdin.readlines()))))
 
