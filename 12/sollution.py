65fdc3663daa14166f1c996d5f595cd26b22e047
blaat <blaat@hotmail.com>
2018-12-12 10:06:12 +0100

    Work as is.

--- 12/sollution.py
+++ 12/sollution.py
@@ -1,7 +1,12 @@
 import sys
 
-state = list(sys.stdin.readline().rstrip().split(' ')[2])
+state = [0,0] + list(map(lambda x: 0 if x == '.' else 1,
+                     list(sys.stdin.readline().rstrip().split(' ')[2])))
+
+print (state)
 
 transfers = list(map(lambda x: (x[0], x[2]),
                      map(lambda x: x.rstrip().split(' '),
                          filter(lambda x: len(x) > 2, sys.stdin.readlines()))))
+
+print (transfers)
