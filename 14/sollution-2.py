5a0ccb7b5b62150451dda886bc94994f20a7fb78
blaat <blaat@hotmail.com>
2018-12-14 13:50:47 +0100

    Fucking Bug

--- 14/sollution-2.py
+++ 14/sollution-2.py
@@ -1,17 +1,18 @@
 import sys
 
 input = str(sys.stdin.readline().rstrip())
-recipes, elves = "37", [0, 1]
+recipes, elves = [3, 7], [0, 1]
 
 def newRecipies(a,b):
-    return str(int(a) + int(b))
+    return list(map(lambda x: int(x), list(str(a+b))))
 
-# Fuck Python and its string addition n^2 complexity.
-while len(recipes) < len(input) or recipes.find(input) == -1:
-    recipes += newRecipies(recipes[elves[0]], recipes[elves[1]])
-    elves = [ (x + 1 + int(recipes[x])) % len(recipes) for x in elves ]
-    print(recipes)
+while len(recipes) < len(input) or "".join(map(lambda x: str(x), recipes[-len(input):])) != input:
+    nr = newRecipies(recipes[elves[0]], recipes[elves[1]])
+    print ("(%s) %d, %d => %s .." % (str(elves), recipes[elves[0]], recipes[elves[1]], str(nr)))
+    for x in nr: recipes.append(x)
+    elves = [ (x + 1 + recipes[x]) % len(recipes) for x in elves ]
+    if len(recipes) % 100000 == 0: print("%d .." % len(recipes))
 pass
 
-print ("Found %s after %d recipes .." % (input, recipes.find(input)))
+print ("Found %s after %d recipes .." % (input, len(recipes) - len(input)))
 
