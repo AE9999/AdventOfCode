bc445ad3ad7296841b4b2e535c03f26083c3604d
blaat <blaat@hotmail.com>
2018-12-18 15:50:55 +0100

    Work as is

--- 18/sollution.py
+++ 18/sollution.py
@@ -4,13 +4,45 @@ import sys
 myInput = open('input-test.dat')
 acres = list(map(lambda x: list(x.rstrip()), myInput.readlines()))
 
-deltas = [(-1, -1), (0,-1), (1,-1), (-1, 0), (1, 0), (-1, 1), (1, 0), (1, 1) ]
+deltas = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
 
 def ajacent(x,y):
     global acres, deltas
     return [acres[y + delta[1]][x + delta[0]] for delta in
-            (filter(lambda d: x + d[0] >= 0 and y + d[1] >= 1,  deltas))]
+            (filter(lambda d: x + d[0] >= 0
+                           and y + d[1] >= 0 \
+                           and x + d[0] < len(acres[0]) \
+                           and y + d[1] < len(acres),  deltas))]
 pass
 
-print ajacent(0,0)
+def printAcres():
+    global acres
+    print("")
+    for line in acres:
+        print "".join(line)
+pass
+
+printAcres()
+for turn in range(1):
+    nextAcres = []
+    for y in range(len(acres)):
+        nrow = []
+        for x in range(len(acres[0])):
+            acre, nacre = acres[y][x], None
+            if acre == '.':
+                nacre = '|' if len(filter(lambda x: x == '|', ajacent(x, y))) >= 3 else '.'
+            elif acre == '|':
+                nacre = '#' if len(filter(lambda x: x == '|', ajacent(x, y))) >= 3 else '|'
+            elif acre == '#':
+                nacre = '#' if len(filter(lambda x: x == '|', ajacent(x, y))) >= 1 \
+                               and len(filter(lambda x: x == '#', ajacent(x, y))) >= 1 else '.'
+            pass
+            nrow.append(nacre)
+        pass
+        nextAcres.append(nrow)
+    pass
+    acres = nextAcres
+    printAcres()
+pass
+
 
