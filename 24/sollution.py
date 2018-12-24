0fe7f1fd8f83e4d271dec68f99b29795d087ef0a
blaat <blaat@hotmail.com>
2018-12-24 11:57:12 +0100

    Work as is.

---
+++ 24/sollution.py
@@ -0,0 +1,17 @@
+import sys, re
+
+class Unit:
+    def __init__(self, line):
+        self.units = 0
+        self.hp = 0
+        self.weaknesses = []
+        self.immunities = []
+        self.damage (0, '')
+        self.initative = 0
+    pass
+pass
+
+# for line in open('input-test.dat').readlines():
+#     print(re.findall(r"\((.+)\)", line))
+
+
